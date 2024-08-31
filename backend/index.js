6504b12220022c119d3e67edfae2fdf6a14dcb7f
aman-gupta-16 <ag8535467@gmail.com>
2024-08-31 11:35:21 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors(corsOption));
+app.use(cors({ origin: "*" }));
 
 //routes
 app.use("/api/v1/user", userRouter);
