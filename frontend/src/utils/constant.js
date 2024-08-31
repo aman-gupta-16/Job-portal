9aaebac4324bf5471670208b3a13e441882f3e9c
aman-gupta-16 <ag8535467@gmail.com>
2024-08-31 11:10:07 +0530

    backend end point change

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,9 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const USER_API_END_POINT = "https://jobvikas-backend2.onrender.com/user";
+export const JOB_API_END_POINT = "https://jobvikas-backend2.onrender.com/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+  "https://jobvikas-backend2.onrender.com/application";
+export const COMPANY_API_END_POINT =
+  "https://jobvikas-backend2.onrender.com/company";
 
 export const calculateDaysAgo = (createdAt) => {
   const now = new Date();
